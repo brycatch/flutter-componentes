e575406e7d1c9ca1c49f2834efeafeab2a16f271
Bryan Alejandro Sanchez Ramirez <sarabryanale@outlook.com>
2020-02-02 12:56:56 -0600

    Página de Avatar

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -9,12 +9,57 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostrat alerta'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () => _mostratAlert(context),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_back),
         onPressed: () {
           Navigator.pop(context);
         },
       ),
+    );
+  }
+
+  void _mostratAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          title: Text('Título'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Contenido'),
+              FlutterLogo(
+                size: 100.0,
+              )
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Cancelar'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            FlatButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -8,6 +8,33 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            child: CircleAvatar(
+              child: ClipOval(
+                child: Image.network(
+                    'https://pm1.narvii.com/7011/b4fc82f32f7d30885d469d9f7718b9ff42c92adcr1-642-421v2_128.jpg'),
+              ),
+            ),
+          ),
+          SizedBox(width: 10.0),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage(
+            'https://dam.smashmexico.com.mx/wp-content/uploads/2018/05/24100112/Stan-Lee.jpg',
+          ),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 100),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_back),
